6fe7cc7bf8c2c58272597a725ae427d83af76b4f
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-07-06 09:09:27 +0530

    m

--- src/Home/MiddleContent.js
+++ src/Home/MiddleContent.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './home.css';
 import { API_ROUTES } from '../app_modules/apiRoutes';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const MiddleContent = () => {
     const [drivingSchools, setDrivingSchools] = useState([]);
@@ -13,27 +15,22 @@ const MiddleContent = () => {
     useEffect(() => {
         const token = localStorage.getItem('token');
 
-        // Fetch all driving schools
         axios.get(API_ROUTES.displayDrivingSchools, {
             params: { token: token }
         })
         .then(response => {
-            console.log('Driving Schools:', response.data); // Debugging log
             setDrivingSchools(response.data);
             setSponsoredSchools(response.data.filter(school => school.sponsored === 1));
         })
         .catch(error => console.error('Error fetching driving schools:', error));
 
-        // Fetch promotions
         axios.get(API_ROUTES.displayPromotions)
         .then(response => {
-            console.log('Promotions:', response.data); // Debugging log
             setPromotions(response.data);
         })
         .catch(error => console.error('Error fetching promotions:', error));
     }, []);
 
-    // Rotate sponsors every 3 seconds
     useEffect(() => {
         const sponsorInterval = setInterval(() => {
             setCurrentSponsorIndex(prevIndex => (prevIndex + 1) % sponsoredSchools.length);
@@ -42,7 +39,6 @@ const MiddleContent = () => {
         return () => clearInterval(sponsorInterval);
     }, [sponsoredSchools.length]);
 
-    // Rotate promotions every 3 seconds
     useEffect(() => {
         const promotionInterval = setInterval(() => {
             setCurrentPromotionIndex(prevIndex => (prevIndex + 1) % promotions.length);
@@ -53,7 +49,7 @@ const MiddleContent = () => {
 
     const renderSponsoredSchool = () => {
         if (sponsoredSchools.length === 0) {
-            return null; // No sponsored schools yet
+            return null;
         }
 
         const currentSponsor = sponsoredSchools[currentSponsorIndex];
@@ -61,13 +57,15 @@ const MiddleContent = () => {
         return (
             <div className="section">
                 <h4>Sponsored School</h4>
-                <div className="school-list sponsored-list">
-                    <div key={currentSponsor.id} className="school-card-sponsored">
+                <div className="carousel">
+                    <div key={currentSponsor.id} className="school-card sponsored">
                         <img src={`${API_ROUTES.displayImg}/${currentSponsor.logo_url}`} alt={currentSponsor.name} className="school-image" />
                         <div className="school-details">
                             <h3>{currentSponsor.name}</h3>
                             <p>{currentSponsor.location}</p>
-                            <button className="view-btn">View</button>
+                            <button className="view-btn">
+                                <FontAwesomeIcon icon={faEye} /> View
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -77,7 +75,7 @@ const MiddleContent = () => {
 
     const renderPromotionBanners = () => {
         if (promotions.length === 0) {
-            return null; // No promotions yet
+            return null;
         }
 
         const currentPromotion = promotions[currentPromotionIndex];
@@ -85,7 +83,7 @@ const MiddleContent = () => {
         return (
             <div className="section">
                 <h4>Promotions</h4>
-                <div className="promotion-list">
+                <div className="carousel">
                     <div key={currentPromotion.id} className="promotion-card">
                         <img src={`${API_ROUTES.displayImg}/${currentPromotion.banner_url}`} alt={currentPromotion.title} className="promotion-image" />
                     </div>
@@ -95,11 +93,12 @@ const MiddleContent = () => {
     };
 
     const renderBestRatedSchools = () => {
-        const bestRatedSchools = drivingSchools.filter(school => school.rating >= 4); // Example criteria for best rated
+        const bestRatedSchools = drivingSchools.filter(school => school.rating >= 4);
+
         return (
             <div className="section">
                 <h4>Best Rated Schools</h4>
-                <div className="school-list-best-schools">
+                <div className="school-list">
                     {bestRatedSchools.map(school => (
                         <div key={school.id} className="school-card">
                             <img src={`${API_ROUTES.displayImg}/${school.logo_url}`} alt={school.name} className="school-image" />
@@ -107,8 +106,12 @@ const MiddleContent = () => {
                                 <h3>{school.name}</h3>
                                 <p>Rating: {school.rating}</p>
                                 <p>Address: {school.address}</p>
-                                <button className="view-btn">View</button>
-                                <button className="like-btn">❤️</button>
+                                <button className="view-btn">
+                                    <FontAwesomeIcon icon={faEye} /> View
+                                </button>
+                                <button className="like-btn">
+                                    <FontAwesomeIcon icon={faHeart} /> Like
+                                </button>
                             </div>
                         </div>
                     ))}
@@ -118,11 +121,10 @@ const MiddleContent = () => {
     };
 
     const renderAllDrivingSchools = () => {
-        console.log('All Driving Schools:', drivingSchools); // Log to check state before rendering
         return (
             <div className="section">
                 <h4>All Driving Schools</h4>
-                <div className="school-list-all-driving-schools">
+                <div className="school-list">
                     {drivingSchools.map(school => (
                         <div key={school.id} className="school-card">
                             <img src={`${API_ROUTES.displayImg}/${school.logo_url}`} alt={school.name} className="school-image" />
@@ -130,8 +132,12 @@ const MiddleContent = () => {
                                 <h3>{school.name}</h3>
                                 <p>Rating: {school.rating}</p>
                                 <p>Address: {school.address}</p>
-                                <button className="view-btn">View</button>
-                                <button className="like-btn">❤️</button>
+                                <button className="view-btn">
+                                    <FontAwesomeIcon icon={faEye} /> View
+                                </button>
+                                <button className="like-btn">
+                                    <FontAwesomeIcon icon={faHeart} /> Like
+                                </button>
                             </div>
                         </div>
                     ))}
