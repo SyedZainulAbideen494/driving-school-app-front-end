6ee10ec0be2b1a5f81d42e79f74740a25edd369e
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-07-08 18:32:23 +0530

    m

--- src/Home/SlidingBar.js
+++ src/Home/SlidingBar.js
@@ -1,18 +1,24 @@
-// SlidingBar.js
 import React from 'react';
-import { FaUser, FaCog, FaSignOutAlt, FaEnvelope, FaInfoCircle } from 'react-icons/fa';
+import { FaUser, FaCog, FaSignOutAlt, FaBell, FaSchool, FaQuestionCircle, FaInfoCircle } from 'react-icons/fa';
 import './SlidingBar.css'; // Import the CSS for the sliding bar
+import { useNavigate } from 'react-router-dom';
 
 const SlidingBar = ({ isOpen, onClose }) => {
+    const nav = useNavigate()
+    const handleredirectAddschool = () => {
+        nav('/register/school/form')
+    }
     return (
         <div className={`sliding-bar ${isOpen ? 'open' : ''}`}>
             <div className="sliding-bar-content">
                 <button className="close-btn" onClick={onClose}>X</button>
                 <button className="sliding-bar-btn"><FaUser /> Profile</button>
+                <button className="sliding-bar-btn" onClick={handleredirectAddschool}><FaSchool /> Add Driving School</button>
                 <button className="sliding-bar-btn"><FaCog /> Settings</button>
-                <button className="sliding-bar-btn"><FaEnvelope /> Messages</button>
-                <button className="sliding-bar-btn"><FaInfoCircle /> About</button>
                 <button className="sliding-bar-btn"><FaSignOutAlt /> Logout</button>
+                <button className="sliding-bar-btn"><FaInfoCircle /> About</button>
+                <button className="sliding-bar-btn"><FaBell /> Notifications</button>
+                <button className="sliding-bar-btn"><FaQuestionCircle /> Help</button>
             </div>
         </div>
     );
